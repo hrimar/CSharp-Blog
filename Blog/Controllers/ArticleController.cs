ef3f059da34ab6760e536dea25f2cc24a48e1f56
hrimar <hhristoff@yahoo.com>
2017-09-02 18:29:09 +0300

    CSharp Blog

---
+++ Blog/Controllers/ArticleController.cs
@@ -0,0 +1,240 @@
+﻿using Blog.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.Entity;
+using System.Net;
+
+namespace Blog.Controllers
+{
+    public class ArticleController : Controller
+    {
+        // GET: Article
+        public ActionResult Index()
+        {
+            return RedirectToAction("List", "Article");
+        }
+
+        // GET: Article/List
+        public ActionResult List()
+        {
+            using (var database = new BlogDbContext())
+            {
+                var articles = database.Articles
+                    .Include(a => a.Author)
+                    .ToList();
+
+                return View(articles);
+            }
+        }
+
+        // GET: Article/Details
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            using (var database = new BlogDbContext())
+            {
+                var article = database.Articles
+                    .Where(a => a.Id == id)
+                    .Include(a => a.Author)
+                    .First();
+
+                if (article == null)
+                {
+                    return HttpNotFound();
+                }
+
+                return View(article);
+            }
+        }
+
+
+        // GET: Article/Create
+        [Authorize]
+        public ActionResult Create()
+        {          
+            return View();
+           
+        }
+
+        // POST: Article/Create
+        [HttpPost]
+        [Authorize]
+        public ActionResult Create(Article article)
+        {
+            if (ModelState.IsValid)
+            {
+                using (var database = new BlogDbContext())
+                {
+                    //Get author id
+                    var authorId = database.Users
+                        .Where(u => u.UserName == this.User.Identity.Name)
+                        .First()
+                        .Id;
+
+                    //Set articles author
+                    article.AuthorId = authorId;
+
+                    // Save article to DB
+                    database.Articles.Add(article);
+                    database.SaveChanges();
+
+                    return RedirectToAction("Index");
+                }
+            }
+
+            return View(article);
+        }
+
+
+        // GET: Article/Delete
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            using (var database = new BlogDbContext())
+            {
+                //Get article from database
+                var article = database.Articles
+                    .Where(a => a.Id == id)
+                    .Include(a => a.Author)
+                    .First();
+
+                if (!IsUserAuthorizedToEdit(article))
+                {
+                    return new HttpStatusCodeResult(HttpStatusCode.Forbidden);
+                }
+
+                // Check if article exists
+                if (article == null)
+                {
+                    return HttpNotFound();
+                }
+
+                // Pass article to view
+                return View(article);
+            }
+        }
+
+
+        // POST: Article/Delete
+        [HttpPost]
+        [ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            using (var database = new BlogDbContext())
+                {
+                    //Get article from database
+                    var article = database.Articles
+                        .Where(a => a.Id == id)
+                        .Include(a => a.Author)
+                        .First();
+
+                // Check if article exists
+                if (article == null)
+                {
+                    return HttpNotFound();
+                }
+
+                // Delete article from DB
+                database.Articles.Remove(article);
+                database.SaveChanges();
+
+                // edirect to index page
+                    return RedirectToAction("Index");
+                }                        
+        }
+
+        //private object Include(Func<object, object> p)
+        //{
+        //    throw new NotImplementedException();
+        //}
+
+        // GET: Article/Edit
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            using (var database = new BlogDbContext())
+            {
+                // Get article from database
+                var article = database.Articles
+                    .Where(a => a.Id == id)
+                    .First();
+
+                if (!IsUserAuthorizedToEdit(article))
+                {
+                    return new HttpStatusCodeResult(HttpStatusCode.Forbidden);
+                }
+
+                // Check if article exists
+                if (article == null)
+                {
+                    return HttpNotFound();
+                }
+
+                // Create the view model
+                var model = new ArticleViewModel();
+                model.Id = article.Id;
+                model.Title = article.Title;
+                model.Description = article.Description;
+
+                // Pass the view model to view
+                return View(article);
+            }
+        }
+
+        // POST: Article/Edit
+        [HttpPost]
+        public ActionResult Edit(ArticleViewModel model)
+        {
+            // Check if model state is valid
+            if (ModelState.IsValid)
+            {
+                using (var database = new BlogDbContext())
+                {
+                    //Get article from database
+                    var article = database.Articles
+                        .FirstOrDefault(a => a.Id == model.Id);
+
+                    // Set article properties
+                    article.Title = model.Title;
+                    article.Description = model.Description;
+
+                    // Save article state in DB
+                    database.Entry(article).State = EntityState.Modified;
+                    database.SaveChanges();
+
+                    // Redirect to index page
+                    return RedirectToAction("Index");
+                }
+            }
+            // If model state is invalid, return the same view
+            return View(model);
+        }
+
+        private bool IsUserAuthorizedToEdit(Article article)
+        {
+            bool isAdmin = this.User.IsInRole("Admin");
+            bool isAuthor = article.IsAuthor(this.User.Identity.Name);
+
+            return isAdmin || isAuthor;
+        }
+    }
+}
---
+++ Blog/Models/Article.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Blog.Models
+{
+    public class Article
+    {      
+        public int Id { get; set; }
+         
+        [Required]
+        [StringLength(50)]
+        public string Title { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        [Required]
+        public DateTime Date { get; set; }
+
+        public virtual ApplicationUser Author { get; set; }
+        public string AuthorId { get; internal set; }
+
+        public Article()
+        {
+            this.Date = DateTime.Now;
+        }
+
+        public bool IsAuthor(string name)
+        {
+            return this.Author.UserName.Equals(name);
+        }
+    }
+}
